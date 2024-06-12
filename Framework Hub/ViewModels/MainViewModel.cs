3fb56b29f4d96ee77d1654bc582897cf2dffecd6
JamesCJ <jamescraddockjones@icloud.com>
2024-06-12 09:41:02 +0100

    Add early Ryzen power control

--- Framework Hub/ViewModels/MainViewModel.cs
+++ Framework Hub/ViewModels/MainViewModel.cs
@@ -3,19 +3,41 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
 using System.Diagnostics;
+using Framework_Hub.Scripts.Windows.RyzenAdj;
+using Framework_Hub.Scripts.Linux.RyzenAdj;
+using System.Runtime.InteropServices;
+using Framework_Hub.Scripts.Windows.Misc;
 
 namespace Framework_Hub.ViewModels
 {
     public class MainViewModel : ReactiveObject
     {
-        private int _pl1;
-        private int _pl2;
+        private int _powerIndex;
+
+        private int _pl1 = 999;
+        private int _pl2 = 999;
+
+        private int _pl1Max = 999;
+        private int _pl2Max = 999;
+        private int _pl1Min = 999;
+        private int _pl2Min = 999;
+
         private int _temp;
+
         private int _allCO;
         private int _gfxCO;
+
         private int _pboOffset;
+
         private int _winPower;
         private string _winPowerText;
+
+        [Reactive]
+        public int PowerIndex
+        {
+            get => _powerIndex;
+            set => this.RaiseAndSetIfChanged(ref _powerIndex, value);
+        }
 
         [Reactive]
         public int Temp
@@ -36,6 +58,32 @@ namespace Framework_Hub.ViewModels
         {
             get => _pl2;
             set => this.RaiseAndSetIfChanged(ref _pl2, value);
+        }
+
+        public int PL1Max
+        {
+            get => _pl1Max;
+            set => this.RaiseAndSetIfChanged(ref _pl1Max, value);
+        }
+
+        [Reactive]
+        public int PL2Max
+        {
+            get => _pl2Max;
+            set => this.RaiseAndSetIfChanged(ref _pl2Max, value);
+        }
+
+        public int PL1Min
+        {
+            get => _pl1Min;
+            set => this.RaiseAndSetIfChanged(ref _pl1Min, value);
+        }
+
+        [Reactive]
+        public int PL2Min
+        {
+            get => _pl2Max;
+            set => this.RaiseAndSetIfChanged(ref _pl2Min, value);
         }
 
         [Reactive]
@@ -75,16 +123,7 @@ namespace Framework_Hub.ViewModels
 
         public MainViewModel()
         {
-            // Set default values
-            PL1 = 35;
-            PL2 = 55;
-            Temp = 100;
-            AllCO = 0;
-            GfxCO = 0;
-            PboOffset = 1;
-            WinPower = 2;
-            WinPowerText = "\uec4a";
-
+            // Set event for power related value changes 
             this.WhenAnyValue(
                 x => x.Temp,
                 x => x.PL1,
@@ -93,14 +132,83 @@ namespace Framework_Hub.ViewModels
                 x => x.GfxCO,
                 x => x.PboOffset,
                 x => x.WinPower
-            ).Subscribe(_ => OnPower());
-        }
+            ).Subscribe(_ => OnPowerChange());
 
-        private void OnPower()
+            PowerIndex = 2;
+            Temp = 100;
+
+            // Setup temp defaults for each power mode
+            SetUpTempPower();
+        }
+        int runs = 0;
+        private void OnPowerChange()
         {
+            // Update Windows power mode icon
             if (WinPower == 0) WinPowerText = "\ue8be";
             else if (WinPower == 1) WinPowerText = "\uec49";
             else if (WinPower == 2) WinPowerText = "\uec4a";
+
+            // RyzenAdj apply code for Windows 
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Set temp limit
+                RyzenAdj_Backend_Windows.set_tctl_temp(RyzenAdj_Backend_Windows.ry, (uint)Temp);
+                // Set PL1
+                RyzenAdj_Backend_Windows.set_slow_limit(RyzenAdj_Backend_Windows.ry, (uint)(PL1 * 1000));
+                // Set PL2
+                RyzenAdj_Backend_Windows.set_fast_limit(RyzenAdj_Backend_Windows.ry, (uint)(PL2 * 1000));
+
+                // Set all core Curve Optimiser offset
+                RyzenAdj_Backend_Windows.set_coall(RyzenAdj_Backend_Windows.ry, (uint)AllCO);
+                // Set iGPU Curve Optimiser offset
+                RyzenAdj_Backend_Windows.set_cogfx(RyzenAdj_Backend_Windows.ry, (uint)GfxCO);
+            }
+            // RyzenAdj apply code for Linux 
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // Set temp limit
+                RyzenAdj_Backend_Linux.set_tctl_temp(RyzenAdj_Backend_Linux.ry, (uint)Temp);
+                // Set PL1
+                RyzenAdj_Backend_Linux.set_slow_limit(RyzenAdj_Backend_Linux.ry, (uint)(PL1 * 1000));
+                // Set PL2
+                RyzenAdj_Backend_Linux.set_fast_limit(RyzenAdj_Backend_Linux.ry, (uint)(PL2 * 1000));
+
+                // Set all core Curve Optimiser offset
+                RyzenAdj_Backend_Linux.set_coall(RyzenAdj_Backend_Linux.ry, (uint)AllCO);
+                // Set iGPU Curve Optimiser offset
+                RyzenAdj_Backend_Linux.set_cogfx(RyzenAdj_Backend_Linux.ry, (uint)GfxCO);
+            }
+        }
+
+        private void SetUpTempPower()
+        {
+            // Setup temp defaults for each power mode
+            if (GetSystemInfo.Product.Contains("16"))
+            {
+                if (PowerIndex == 0)
+                {
+                    PL1 = 85;
+                    PL2 = 85;
+                    WinPower = 0;
+                }
+                else if (PowerIndex == 1)
+                {
+                    PL1 = 95;
+                    PL2 = 95;
+                    WinPower = 1;
+                }
+                else if (PowerIndex == 2)
+                {
+                    PL1 = 100;
+                    PL2 = 120;
+                    WinPower = 2;
+                }
+
+                PL1Min = 10;
+                PL2Min = 10;
+                PL1Max = 140;
+                PL2Max = 140;
+            }
         }
     }
 }
--- Framework Hub/Views/MainView.axaml.cs
+++ Framework Hub/Views/MainView.axaml.cs
@@ -18,47 +18,44 @@ public partial class MainView : UserControl
         InitializeComponent();
         DataContext = new MainViewModel();
 
+        // Setup GUI
         SetUpGUI();
     }
 
     private void SetUpGUI()
     {
+        // Create variable containing viewmodel for GUI
         var mainViewModel = new MainViewModel();
 
+        // Setup power mode selection menu
         lbPresets.Items.Add(new SideMenu { Icon = "\ue8be", Sub = "Silent", Margin = new Thickness(0, -8, 0, -8) });
         lbPresets.Items.Add(new SideMenu { Icon = "\uec49", Sub = "Balanced", Margin = new Thickness(0, -8, 0, -8) });
         lbPresets.Items.Add(new SideMenu { Icon = "\uec4a", Sub = "Performance", Margin = new Thickness(0, -8, 0, -8) });
-        lbPresets.SelectedIndex = 2;
 
+        // Display laptop name
         tbxLaptopName.Text = $"{GetSystemInfo.Manufacturer} {GetSystemInfo.Product}";
+
+        // Display CPU/APU name
         tbxCpuName.Text = $"- {GetSystemInfo.GetCPUName()}";
 
+        // Determine how many GPUs are present, their order and display their names
         if(GetSystemInfo.GetGPUName(1) != null && GetSystemInfo.GetCPUName().Contains(GetSystemInfo.GetGPUName(1).Replace("AMD", null).Replace("(TM)", null))) tbxGpuName.Text = $"- {GetSystemInfo.GetGPUName(1)} + {GetSystemInfo.GetGPUName(0)}";
         else if (GetSystemInfo.GetGPUName(1) != null) tbxGpuName.Text = $"- {GetSystemInfo.GetGPUName(0)} + {GetSystemInfo.GetGPUName(1)}";
         else tbxGpuName.Text = $"- {GetSystemInfo.GetGPUName(0)}";
 
+        // Display RAM spec of laptop
         GetSystemInfo.GetRAMInfo(tbxRamSpecs);
 
-        if (GetSystemInfo.Product.Contains("16"))
+        // Detect if Ryzen 9/Ryzen AI 9 is present
+        if (!GetSystemInfo.GetCPUName().Contains("Ryzen 9") && !GetSystemInfo.GetCPUName().Contains("Ryzen AI 9"))
         {
-            sdPL1.Maximum = 140;
-            sdPL2.Maximum = 140;
-
-            if (lbPresets.SelectedIndex == 1)
-            {
-                sdPL1.Value = 85;
-                sdPL2.Value = 85;
-            }
-            else if (lbPresets.SelectedIndex == 1)
-            {
-                sdPL1.Value = 95;
-                sdPL2.Value = 95;
-            }
-            else if (lbPresets.SelectedIndex == 2)
-            {
-                sdPL1.Value = 100;
-                sdPL2.Value = 120;
-            }
+            // Hide options if no Ryzen 9/Ryzen AI 9 is detected
+            expCO.IsVisible = false;
+            expPBO.IsVisible = false;
         }
+
+        sdPL1.Value = mainViewModel.PL1;
+        sdPL2.Value = mainViewModel.PL2;
+        lbPresets.SelectedIndex = mainViewModel.PowerIndex;
     }
 }
